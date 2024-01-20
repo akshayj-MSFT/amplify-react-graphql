c8882f3eb272eb56598ca364ea3ed604077ce704
Akshay Johar <ajohar@gmail.com>
2024-01-19 17:16:24 -0800

    Update Login page with Image

--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import background from "./img/GregoryBackground.png";
+import logo from "./img/GregoryIcon.png";
 
 function Login() {
 
@@ -37,20 +38,23 @@ function Login() {
 
   return (
     <div className="d-flex vh-100 justify-content-center align-items-center" style={{ backgroundImage: `url(${background})` }}>
-        <div className="p-3 bg-secondary w-25" >
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" placeholder="Enter Email" id="email" name="email" className="form-control" onChange={(event) => { setEmail(event.target.value); setIsAuthenticated("true");}}></input>                    
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="pwd">Password:</label>
-                    <input type="password" placeholder="Enter Password" id="pwd" name="pwd" className="form-control" onChange={(event) => { setPwd(event.target.value); setIsAuthenticated("true");}}></input>                    
-                </div>
-                <button className="btn btn-primary">Login</button>
-                {(isAuthenticated == "false")  ? <div className="text-warning">Incorrect Username or Password</div> : <div></div>}
-            </form>
-      </div>
+        <div className="container-div w-25">
+        <img src={logo} alt="Gregory Logo" style={{ alignSelf: 'center' }}  />            
+            <div className="p-3 bg-secondary" >
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" placeholder="Enter Email" id="email" name="email" className="form-control" onChange={(event) => { setEmail(event.target.value); setIsAuthenticated("true");}}></input>                    
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="pwd">Password:</label>
+                        <input type="password" placeholder="Enter Password" id="pwd" name="pwd" className="form-control" onChange={(event) => { setPwd(event.target.value); setIsAuthenticated("true");}}></input>                    
+                    </div>
+                    <button className="btn btn-primary">Login</button>
+                    {(isAuthenticated == "false")  ? <div className="text-warning">Incorrect Username or Password</div> : <div></div>}
+                </form>
+            </div>
+      </div>      
     </div>
   );
 }
